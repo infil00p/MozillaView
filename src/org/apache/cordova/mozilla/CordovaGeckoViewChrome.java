c90148ef9e05f29b9f8bad6db86531a8abba3d34
Joe Bowser <bowserj@apache.org>
2014-08-19 16:30:37 -0700

    Force this visible, although we should make it visible when the page is loaded, and not before.

---
+++ src/org/apache/cordova/mozilla/CordovaGeckoViewChrome.java
@@ -0,0 +1,27 @@
+package org.apache.cordova.mozilla;
+
+import org.apache.cordova.Config;
+import org.mozilla.gecko.GeckoView;
+import org.mozilla.gecko.GeckoViewChrome;
+import org.mozilla.gecko.PrefsHelper;
+
+import android.util.Log;
+import android.view.View;
+
+
+public class CordovaGeckoViewChrome extends GeckoViewChrome {
+    
+    String LOGTAG = "CordovaGeckoViewChrome";
+    
+    public void onReady(GeckoView view) {
+        Log.i(LOGTAG, "Gecko is ready");
+
+        PrefsHelper.setPref("devtools.debugger.remote-enabled", true);
+
+        /* Load URL does nothing, we have to wait unitl things are ready before loading */
+        view.addBrowser(Config.getStartUrl());
+        //Make sure this is visible regardless of what Cordova does.
+        view.setVisibility(View.VISIBLE);
+    }
+
+}
